ec8e3256f80c93f578be37442b1cf70b63e05efa
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2023-07-08 18:41:16 -0700

    finish after line 41 UP and after line 45 left

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import random
+
 import pygame 
-from pygame.constants import QUIT
+from pygame.constants import QUIT, K_DOWN, K_UP, K_LEFT, K_RIGHT 
 
 pygame.init()
 
@@ -17,33 +19,49 @@ player_size = (20, 20)
 player = pygame.Surface((player_size))
 player.fill((COLOR_WHITE))
 player_rect = player.get_rect()
-player_speed = [1, 1]
+#player_speed = [1, 1]
+player_move_down = [0, 1]
+player_move_right = [1, 0]
 
 playing = True
 
 while playing:
     FPS.tick(120)
+
     for event in pygame.event.get():
         if event.type == QUIT:
             playing = False
 
-main_display.fill(COLOR_BLACK)
+        main_display.fill(COLOR_BLACK)
+
+        keys = pygame.key.get_pressed()
+
+        if keys[K_DOWN] and player_rect.bottom < HEIGHT:
+            player_rect = player_rect.move(player_move_down)
 
 
-if player_rect.bottom >= HEIGHT:
-    player_speed = [1, -1]
+        if keys[K_RIGHT] and player_rect.right < WIDTH:
+            player_rect = player_rect.move(player_move_right)
 
-if player_rect.top <= 0:
-    player_speed = [-1, 1]
+        # if player_rect.bottom >= HEIGHT:
+        #     player_speed = random.choice(( [1, -1], [-1, -1]))
 
-if player_rect.right >= WIDTH:
-    player_speed = [-1, -1]
+        # if player_rect.top <= 0:
+        #     player_speed = random.choice(( [-1, 1], [1, 1]))
 
-if player_rect.left <= 0:
-    player_speed = [1, 1]
+        # if player_rect.right >= WIDTH:
+        #     player_speed = random.choice(( [-1, -1], [-1, 1]))
 
-main_display.blit(player, player_rect)
+        # if player_rect.left <= 0:
+        #     player_speed = random.choice(( [1, 1], [1, -1]))
 
-player_rect = player_rect.move(player_speed)
+        main_display.blit(player, player_rect)
 
-pygame.display.flip()
+        #player_rect = player_rect.move(player_speed)
+
+        pygame.display.flip()
+
+
+
+
+
